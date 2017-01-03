5a96d579d923caf50a83086cda33b6c62e94b4ff
Franco Montenegro <franco.montenegro@hashdog.com>
2017-01-02 22:40:41 -0300

    Add symbol type

---
+++ src/main/java/ruke/vrj/symbol/SymbolType.java
@@ -0,0 +1,13 @@
+package ruke.vrj.symbol;
+
+/**
+ * Created by Ruke on 09/10/2016.
+ */
+public enum SymbolType {
+    TYPE,
+    VARIABLE,
+    FUNCTION,
+    STRUCT,
+    LIBRARY,
+    OPERATOR_OVERLOADING
+}
